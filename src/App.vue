88d2fd0f79f57b5209f5d2fb6d53c2e46ef85627
Dalton Carr <daltonlcarr@yahoo.com>
2019-01-18 17:05:14 -0800

    initial commit with basic voice recog working with vue-speech npm

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to Translation!</h1>
+    <Translate msg="Hello"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Translate from './components/Translate.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Translate
   }
 }
 </script>
---
+++ src/components/Translate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="translate">
+      <h1>{{ msg }}</h1>
+      <vue-speech @onTranscriptionEnd="onEnd"/>
+    <button v-on:click="OnEnd()">Click me</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueSpeech from 'vue-speech';
+Vue.use(VueSpeech);
+
+export default {
+  name: 'Translate',
+  props: {
+    msg: String
+  },
+  methods: {
+    onEnd({ lastSentence, transcription}){
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
